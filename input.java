981cd8e3fc47336b79fcdd7f79872109093e3a4a
Yash Raj <yash46071@gmail.com>
2023-07-19 09:48:12 +0530

    java basic code

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//code for taking name as an input from user and print; 
+public class input{
+    public static void main(String arg[]){
+        System.out.println("enter the name:");
+        Scanner sc = new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.println("name is "+ name);
+        
+    }
+    
+}
