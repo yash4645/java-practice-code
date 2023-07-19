a45c1243fc829622c41f318536516e3765abcc7f
Yash Raj <yash46071@gmail.com>
2023-07-19 09:46:30 +0530

    java code

---
+++ hallo.java
@@ -0,0 +1,10 @@
+import java. util.*;
+//code for any line of string thst user wants to print;
+public class hallo{
+     public static void main(String arg[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter whatever u want to print");
+         String name =sc.nextLine();
+         System.out.println("hii i am "+name);
+     }
+}
