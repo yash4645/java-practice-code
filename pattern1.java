79dac7786a6983c6c44a9108d744085f378f0b04
Yash Raj <yash46071@gmail.com>
2023-07-20 20:28:31 +0530

    java pattern code

---
+++ pattern1.java
@@ -0,0 +1,12 @@
+//code for printing pattern;
+public class pattern1 {
+    public static void main(String[] args) {
+        for(int i = 4;i>=1;i--){
+            for(int j= i; j>=1;j--){
+                System.out.print("*  ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
